280b734491d55ec104b028fa80492a85043d665d
Victor Dibia <victor.dibia@gmail.com>
2020-10-14 13:31:40 -0700

    update build

---
+++ app/build/precache-manifest.da5a26e9ef35ad11d605673ab03b1749.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96498c5585b38b0dcab41a4494633263",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ce1a7b524436ba0c097",
+    "url": "/static/css/main.8943d821.chunk.css"
+  },
+  {
+    "revision": "39ddb606f67d9d136689",
+    "url": "/static/js/2.4a76fb14.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.4a76fb14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce1a7b524436ba0c097",
+    "url": "/static/js/main.82725c93.chunk.js"
+  },
+  {
+    "revision": "6c2bd227e49ce4801d7b81a80e7e5550",
+    "url": "/static/js/main.82725c93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
--- app/build/service-worker.js
+++ app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e5ef6d463223f4cbd7be7e04ba1c2ec.js"
+  "/precache-manifest.da5a26e9ef35ad11d605673ab03b1749.js"
 );
 
 self.addEventListener('message', (event) => {
