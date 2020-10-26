48da1a2ecbbc2b86d9b9312a4d10a723eac5f4fb
Victor Dibia <victor.dibia@gmail.com>
2020-10-25 20:48:31 -0700

    add tests for extractor

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,13 +1,23 @@
 from lib import Extractor
 from lib import Model
+from lib.utils import image_to_np_array
 import numpy as np
 
 
-def test_extract():
-    efficientnet_model = Model(model_name="efficientnetb0")
-    sample_image_path = "app/build/assets/semsearch/datasets/fashion200/0.jpg"
-    images_dir = "app/build/assets/semsearch/datasets/fashion200/"
-    extractor = Extractor()
+images_dir = "app/build/assets/semsearch/datasets/fashion200/"
+sample_image_path = images_dir + "/0.jpg"
+efficientnet_model = Model(model_name="efficientnetb0")
+extractor = Extractor()
 
+
+def test_extract_dir():
     features, ids = extractor.extract_from_dir(images_dir, efficientnet_model)
-    print(ids)
+    print(features.shape)
+    assert len(ids) == 200 and features.shape == (200, 62720)
+
+
+def test_extract():
+    image_array = image_to_np_array(sample_image_path, 224)
+    image_array = np.asarray([image_array])
+    features = extractor.extract(image_array, efficientnet_model)
+    assert features.shape == (1, 62720)
